2d42163dfda9992bec38c5a229004c6b97379aa7
Ivan Smekchnev <49167389+Linxum@users.noreply.github.com>
2020-09-05 17:43:12 +0300

    Update munch.py
    Added more custom cards, "Action card" and upgrade "Wash" function

--- munch.py
+++ munch.py
@@ -16,49 +16,53 @@ count_user = len(user)
 bool = True
 card_user = [[], [], [], []]
 
-door = [["навальный", "лукашенко", "медведев", "путин", "бэтмен"], ["пожилая чокопайка", "каво-2", "олежа навальный", "карнавальный", "”], [“поцелуй Брежнева”, “”, “”, “”, “”], [“плоти нологи”, “денег нет, но вы держитесь”, “”, “”, “”, “”], ]
-riches = [[“бротофан”, “wargaming”, “любэ”, “”], [“коммандирово4ка Ельцина”, “”, “”, “”, “”], [“маска для сна”, “брови Брежнева”, “”, “”, “”], [“зубр”, “мишка”, “”, “”, “”, “”], [“палёная б/ушная колонка JBL”, “бэтаранг”]]
+doors = [["навальный", "лукашенко", "медведев", "путин", "бэтмен"], ["пожилая чокопайка", "каво-2", "олежа навальный", "карнавальный", "q"], ["поцелуй Брежнева", "q", "q", "q", "q"], ["плоти нологи", "денег нет, но вы держитесь", "q", "q", "q"]]
+riches = [["бротофан", "wargaming", "любэ", "q", "q"], ["коммандирово4ка Ельцина", "q", "q", "q", "q"], ["маска для сна", "брови Брежнева", "q", "q", "q"], ["зубр", "мишка", "q", "q", "q"], ["палёная б/ушная колонка JBL", "бэтаранг", "q", "q", "q"]] 
 
 def cardsbegin(x):
 	for i in range(x):
-		c = random.randint(0, 8)
+		c = random.randint(0, 1)
 		for it in range(0, count_user):
 			n = random.randint(0, 4)
+			doo = int(random.randint(0, 3))
+			rich = int(random.randint(0, 4))
 			if c == 0:
-				card_user[it].append(a[n])
-			elif c == 1:
-				card_user[it].append(b[n])
-			elif c == 2:
-				card_user[it].append(c[n])
-			elif c == 3:
-				card_user[it].append(d[n])
-			elif c == 4:
-				card_user[it].append(e[n])
-			elif c == 5:
-				card_user[it].append(g[n])
-			elif c == 6:
-				card_user[it].append(f[n])
-			elif c == 7:
-				card_user[it].append(h[n])
-			elif c == 8:
-				card_user[it].append(i[n])
-
+				chosen_card = doors[doo][n]
+				card_user[it].append(chosen_card)
+			else:
+				chosen_card = riches[rich][n]
+				card_user[it].append(chosen_card)
+			
 def begin():
 	cardsbegin(count_card)
 	for i in range(0, count_user):
-		print("Карты " + user[i] + " : " + str(card_user[i]))
+		print("Карты " + user[i] + " : " + str(', '.join(card_user[i])))
 
 def chkcnt():
 	for i in range(0, count_user):
 		while len(card_user[i]) > 5:
 			d = int(input(user[i] + ", введи номер карты, которую не жалко: ")) - 1
 			del card_user[i][d]
-			print("Теперь у тебя: " + str(card_user[i]))
+			print("Теперь у тебя: " + str(', '.join(card_user[i])))
 
 def door():
 	print("Check")
-	open_door = a[random.randint(0, 4)]
-	print("Ты открыл дверь. Тебе выпало: " + open_door)
+	open_door = doors[random.randint(0, 3)][random.randint(0, 4)]
+	print("Ты открыл дверь. Тебе выпало: " + str(open_door))
+	actions()
+
+def actions():
+	action = input("Твои действия: (С)мывка; (П)омощь друга; (К)арты: ")
+	if action == "С" or action == "с" or action == "c" or action == "C":
+		square = random.randint(1, 6)
+		print("Тебе выпало: " + str(square))
+		if square > 4 :
+			print("Тебе везет сегодня!")
+		else:
+			print("Не повезло")
+	elif action == "К" or action == "к" or action == "K" or action == "k":
+		print("Твои карты: " + str(', '.join(card_user[0])))
+		chosen_card = input("Выбери карту действия: ")
 
 begin()
 chkcnt()
